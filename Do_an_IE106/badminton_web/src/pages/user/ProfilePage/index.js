14fa8bed822a9f98c7a9ebcf21f3c46cea3483c9
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-06 23:59:10 +0700

    Set up Router

---
+++ Do_an_IE106/badminton_web/src/pages/user/ProfilePage/index.js
@@ -0,0 +1,7 @@
+import {memo} from 'react'
+
+const ProPage = () =>{
+    return <h1>Profile Page</h1>
+};
+
+export default memo(ProPage);
---
+++ Do_an_IE106/badminton_web/src/router.js
@@ -0,0 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+import { ROUTERS } from "./utils/router";
+import HomePage from "./pages/user/homePage";
+import ProPage  from "./pages/user/ProfilePage"
+import Masterlayout from "./pages/user/homePage/theme/masterlayout";
+
+const renderUserRouter =() =>{
+    const userRouters =[
+        {
+            path: ROUTERS.USER.HOME,
+            component: HomePage
+        },
+        {
+            path: ROUTERS.USER.PROFILE,
+            component: ProPage
+        }
+    ];
+
+    return(
+        <Masterlayout>
+            <Routes>{
+                    userRouters.map((item, key) => 
+                    (<Route key ={key} path ={item.path} element={<item.component/>} />))
+                }
+            </Routes>
+        </Masterlayout>
+    );
+};
+
+const RouterCustom =() => {
+    return renderUserRouter();
+}
+
+export default RouterCustom;
