6b8fea558193081031912510669cfc2df4c626ea
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-07 17:32:10 +0700

    Top header

--- Do_an_IE106/badminton_web/src/pages/user/theme/header/index.js
+++ Do_an_IE106/badminton_web/src/pages/user/theme/header/index.js
@@ -5,13 +5,25 @@ import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 import { BsTwitter } from "react-icons/bs";
 import { BsFillPersonFill } from "react-icons/bs";
+import { AiFillMail } from "react-icons/ai";
+
 
 const Header = () =>{
     return (
         <div className='header_top'>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-6'>Trai</div>
+                    <div className='col-6 header_top_left'>
+                        <ul>
+                            <li>
+                                <AiFillMail />
+                                hello @gmail.com
+                            </li>
+                            <li>
+                                Miễn phí ship đơn từ 200.000 đ
+                            </li>
+                        </ul>
+                    </div>
                     <div className='col-6 header_top_right'>
                         <ul>
                             <li><BsFacebook /></li>
