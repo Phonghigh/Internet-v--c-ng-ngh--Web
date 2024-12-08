06943785460cdfefc1e374bff2bfa92ac9993981
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-08 22:42:11 +0700

    Nav cơ bản

--- Do_an_IE106/badminton_web/src/pages/user/theme/header/index.js
+++ Do_an_IE106/badminton_web/src/pages/user/theme/header/index.js
@@ -6,9 +6,52 @@ import { BsInstagram } from "react-icons/bs";
 import { BsTwitter } from "react-icons/bs";
 import { BsFillPersonFill } from "react-icons/bs";
 import { AiFillMail } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import { ROUTERS } from '../../../../utils/router';
+import React, { useState } from 'react';
+
 
 
 const Header = () =>{
+    const [menus, setMenus] = useState ([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME
+        },
+        {
+            name: "Cửa Hàng",
+            path: ROUTERS.USER.PRODUCTS
+        },
+        {
+            name: "Sản phẩm",
+            path: "",
+            isShowSubmenu: false,
+            child: [
+                {
+                    name: "Vợt cầu lông",
+                    path: "",
+                },
+                {
+                    name: "Giày cầu lông",
+                    path: "",
+                },
+                {
+                    name: "Lưới cầu lông",
+                    path: "",
+                },
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: ""
+        },
+        {
+            name: "Liên hệ",
+            path: ""
+        },
+    ])
+    
     return (
     <>
         <div className='header_top'>
@@ -40,17 +83,50 @@ const Header = () =>{
                 </div>
             </div>
         </div>
-        <div className='row'>
-            <div className='col-xl-3'>
-            LOGO
+        <div className='container'>
+            <div className='row'>
+                <div className='col-xl-3'>
+                    <div className='header_logo'>
+                        <h1>Smash Shop</h1>
+                    </div>
+                </div>
+                <div className='col-xl-6'>
+                    <div className='header_menu'>
+                        <ul>
+                            {menus?.map((menu,menuKey)=>(
+                                    <li key={menuKey} className={menuKey === 0 ? "active": ""}>                                        <Link to ={menu?.path}>: 
+                                            {menu?.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            {/* <li>
+                                
+                                <ul>
+                                    <li>Vợt cầu lông</li>
+                                    <li>Vợt cầu lông</li>
+                                    <li>Vợt cầu lông</li>
+                                </ul>
+                            </li> */}
+                        </ul>
+                    </div>
+                </div>
+                <div className='col-xl-3'>
+                    <div className='header_cart'>
+                        <div className='header_cart_price'>
+                            <span>
+                                100 đ
+                            </span>
+                        </div>
+                        <ul>
+                            <li>
+                                <Link to ='#'>
+                                <AiOutlineShoppingCart /> <span>5</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className='col-xl-6'>
-            MENU
-            </div>
-            <div className='col-xl-3'>
-            PHONE
-            </div>
-
         </div>
     </>
     );
--- Do_an_IE106/badminton_web/src/utils/router.js
+++ Do_an_IE106/badminton_web/src/utils/router.js
@@ -2,5 +2,7 @@ export const ROUTERS ={
     USER: {
         HOME: "",
         PROFILE: 'thong-tin-ca-nhan',
+        PRODUCTS: '/san-pham',
+
     }
 }
