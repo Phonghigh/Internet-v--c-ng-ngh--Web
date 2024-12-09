68cf7a60d394f344417779da7dbaa4bf01a065ce
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-10 02:38:27 +0700

    Sản phẩm nổi bật: cơ bản

--- Do_an_IE106/badminton_web/src/pages/user/homePage/index.js
+++ Do_an_IE106/badminton_web/src/pages/user/homePage/index.js
@@ -1,8 +1,11 @@
 import {memo} from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-// import cat1Img from "/assets/user/images/categories/cat-1.webp"
+import feature1Img from "../../../assets/user/images/feature/feature-1.jpg"
+import feature2Img from "../../../assets/user/images/feature/feature-2.jpg"
+import feature3Img from "../../../assets/user/images/feature/feature-3.jpg"
 import "./style.scss";
+import { Tabs,TabList,TabPanel,Tab } from 'react-tabs';
 
 const HomePage = () =>{
     const responsive = {
@@ -24,6 +27,71 @@ const HomePage = () =>{
             items: 1
         }
     };
+    const featproducts ={
+        all: {
+            title: "Toàn bộ",
+            products: [
+                {
+                    img: feature1Img,
+                    name: "LI-NING AXFORCE BLAST",
+                    price: 2000000,
+                },
+                {
+                    img: feature2Img,
+                    name: "LI-NING JIMMY BUTLER 2 HALO",
+                    price: 1000000,
+                },
+                {
+                    img: feature3Img,
+                    name: "LI-NING NO.3 BOOST STRING",
+                    price: 150000,
+                },
+            ],
+        },
+        racket: {
+            title: "Vợt cầu lông",
+            products: [
+                {
+                    img: feature1Img,
+                    name: "LI-NING AXFORCE BLAST",
+                    price: 2000000,
+                },
+                
+            ],
+        },
+    }
+
+    const renderFeatureProduct = (data) => {
+        const tablist = [];
+        const tabPanels = [];
+    
+        Object.keys(data).forEach((key, index) => {
+            tablist.push(<Tab key={index}>{data[key].title}</Tab>);
+            
+            // Tạo nội dung cho từng tabPanel
+            const tabPanel = data[key].products.map((item, j) => (
+                <div key={j} className="featured_product_item">
+                    <img src={item.img} alt={item.name} />
+                    <p>{item.name}</p>
+                    <p>{item.price.toLocaleString()} VND</p>
+                </div>
+            ));
+            tabPanels.push(tabPanel);
+        });
+    
+        return (
+            <Tabs>
+                <TabList>{tablist}</TabList>
+                {tabPanels.map((content, key) => (
+                    <TabPanel key={key}>
+                        <div className="featured_product_container">
+                            {content}
+                        </div>
+                    </TabPanel>
+                ))}
+            </Tabs>
+        );
+    };
     return (
     <>
         <div className='container container_categories_slider'>
@@ -44,6 +112,14 @@ const HomePage = () =>{
                     <p>Túi cầu lông</p>
                 </div>
             </Carousel>
+        </div>
+        <div className='container'>
+            <div className='featured'>
+                <div className="section_title">
+                    <h2>Sản Phẩm Nổi Bật</h2>
+                </div>
+                {renderFeatureProduct(featproducts)}
+            </div>
         </div>
     </>
     )
