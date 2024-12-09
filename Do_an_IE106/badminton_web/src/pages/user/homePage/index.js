3f10b7da7f0cf54ebbd599c720cc9d09d6871554
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-10 00:57:14 +0700

    homepage slider: hoàn chỉnh

--- Do_an_IE106/badminton_web/src/pages/user/homePage/index.js
+++ Do_an_IE106/badminton_web/src/pages/user/homePage/index.js
@@ -1,7 +1,49 @@
 import {memo} from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+// import cat1Img from "/assets/user/images/categories/cat-1.webp"
+import "./style.scss";
 
 const HomePage = () =>{
-    return <h1>fasdf</h1>
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+    return (
+    <>
+        <div className='container container_categories_slider'>
+            <Carousel responsive={responsive} className='categories_slider'>
+                <div className='categories_slider_item' id='item_1'>
+                    <p>Vợt cầu lông</p>
+                </div>
+                <div className='categories_slider_item' id='item_2'>
+                    <p>Giày cầu lông</p>
+                </div>
+                <div className='categories_slider_item' id='item_3'>
+                    <p>Quấn cán</p>
+                </div>
+                <div className='categories_slider_item' id='item_4'>
+                    <p>Lưới cầu lông</p>
+                </div>
+            </Carousel>
+        </div>
+    </>
+    )
 };
 
 export default memo(HomePage);
